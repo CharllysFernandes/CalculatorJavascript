4930bbe8b3aa6037b34ddb42299ba84513c8876d
Charllys Fernandes <charllys.daiany@gmail.com>
2022-12-27 17:02:56 -0300

    add function calculate.

--- index.js
+++ index.js
@@ -1,44 +1,89 @@
 
-let sign = "";
 let keyboard = document.getElementById("keyboard");
-let buttonArray = ["AC", "+/-", "%","÷",7,8,9,"x",4,5,6,"—",1,2,3,"+","?",0,".","="];
+let buttonArray = ["clear", "plusMinus", "percent", "divider", "seven", "eight", "nine", "eks", "four", "five", "six", "minus", "one", "two", "three", "plus", "return", "zero", "point", "equal"];
 for (let i = 0; i < buttonArray.length; i++) {
     keyboard.innerHTML +=
+        `
+    <button class="color-button-keyboard btn rounded rounded-3 text-white ${buttonArray[i]}" id="${buttonArray[i]}" onclick=key("${buttonArray[i]}")></button>
     `
-    <button class="color-button-keyboard btn rounded rounded-3 text-white " id="${buttonArray[i]}">${buttonArray[i]}</button>
-    `   
 }
-const digits = document.getElementById("digits");
-let firstValue = true;
+// All buttons
+let n1 = "";
+let n2 = "";
+let operator = "";
+let digits = document.getElementById("digits")
+let historic = document.getElementById("history")
+function key(caract) {
+    let keys = document.getElementById(caract); // get button
+    let styles = window.getComputedStyle(keys, "::after");
+    let  buttonContent = styles.content;
+    // console.log(buttonContent)
+    let key = buttonContent.replace(/\D/g, '');
+    console.log(`apenas antes ` + key)
 
-function clear() {
-    digits.innerHTML = ""
-}
-
-let button = document.getElementsByTagName("button");
-
-for (let i = 0; i < button.length; i++) {
-    
-    button[i].onclick = function () {
-        if (firstValue) {
-            clear();
-            firstValue = false;
-            if (this.id >= 0 && this.id <= 9) {
-                digit = this.id
-                digits.innerHTML = digit;
-                console.log(this.id)
-               }
+    if (key >= 0 && key <=9) {
+        console.log(`apenas teste ` + key)
+        if(digits.innerHTML === "0"){
+           digits.innerHTML = key
         }else{
-            if (this.id >= 0 && this.id <= 9) {
-                digit = this.id
-                digits.innerHTML += digit;
-                console.log(this.id)
-            }
+           digits.innerHTML += key
         }
+    }    
+}
+
+document.getElementById("plus").addEventListener('click', function () {
+    n1 = digits.innerHTML;
+    historic.innerHTML = n1 + `<span> + </span>`
+    operator = "plus"
+    digits.innerHTML = "0"
+ 
+})
+document.getElementById("minus").addEventListener('click', function () {
+    n1 = digits.innerHTML;
+    historic.innerHTML = n1 + `<span> - </span>`
+    operator = "minus"
+    digits.innerHTML = "0"
+ 
+})
+document.getElementById("divider").addEventListener('click', function () {
+    n1 = digits.innerHTML;
+    historic.innerHTML = n1 + `<span> ÷ </span>`
+    operator = "divider"
+    digits.innerHTML = "0"
+ 
+})
+document.getElementById("eks").addEventListener('click', function () {
+    n1 = digits.innerHTML;
+    historic.innerHTML = n1 + `<span> x </span>`
+    operator = "eks"
+    digits.innerHTML = "0"
+ 
+})
+
+document.getElementById("equal").addEventListener("click", function () {
+    n2 = digits.innerHTML;
+    historic.innerHTML += n2
+    digits.innerHTML = calculate(n1, operator, n2);
+})
+
+document.getElementById("clear").addEventListener("click", function () {
+    digits.innerHTML = "0"
+})
+function calculate(n1, operator, n2) {
+    switch (operator) {
+        case "plus":
+            return parseFloat(n1) + parseFloat(n2);
+            break;
+        case "minus":
+            return parseFloat(n1) - parseFloat(n2);
+            break
+        case "divider":
+            return parseFloat(n1) / parseFloat(n2);
+            break
+        case "eks":
+            return parseFloat(n1) * parseFloat(n2);
+            break
+        default:
+            break;
     }
 }
-
-document.getElementById("+").addEventListener('click', function () {
-    sign = "+"
-    console.log(sign)
-})
